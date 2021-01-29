fad8e1ab6dbea3d0a93bf94027cd7801184c95f7
Igor Menezes <igor.meneal@gmail.com>
2021-01-29 12:26:47 -0300

    Updated list.

--- src/page/List/index.tsx
+++ src/page/List/index.tsx
@@ -8,6 +8,9 @@ import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 
+import formatCurrency from '../../utils/formatCurrency'
+import formatDate from '../../utils/formatDate'
+
 interface IRouteParams {
   match:{
     params:{
@@ -28,6 +31,8 @@ interface IData {
 const List: React.FC<IRouteParams> = ({match}) => {
 
   const [data, setData] = useState<IData[]>([])
+  const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() +1 ))
+  const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()))
 
   const {type} = match.params;
 
@@ -40,40 +45,59 @@ const List: React.FC<IRouteParams> = ({match}) => {
   },[type]);
 
   const listaData = useMemo(() =>{
-    return type === 'entry-balance' ? gains : expenses 
+    return type === 'entry-balance' ? gains : expenses
   },[type])
 
-
   const months = [
+    {value: 1,label: 'Janeiro'},
+    {value: 2,label: 'Fevereiro'},
+    {value: 3,label: 'Março'},
+    {value: 4,label: 'Abril'},
+    {value: 5,label: 'Maio'},
+    {value: 6,label: 'Junho'},
     {value: 7,label: 'Julho'},
     {value: 8,label: 'Agosto'},
-    {value: 9,label: 'Setembro'}
+    {value: 1,label: 'Janeiro'},
+    {value: 9,label: 'Setembro'},
+    {value: 10,label: 'Outubro'},
+    {value: 11,label: 'Novembro'},
+    {value: 12,label: 'Dezembro'},
   ];
 
   const years = [
+    {value: 2021,label: 2021},
     {value: 2020,label: 2020},
     {value: 2019,label: 2019},
     {value: 2018,label: 2018}
   ];
 
   useEffect(() =>{
-    const response = listaData.map(item=>{
+    const filteredData = listaData.filter(item=>{
+      const date = new Date(item.date);
+      const month = String(date.getMonth() +1);
+      const year = String(date.getFullYear());
+
+      return month === monthSelected && year === yearSelected;
+    });
+
+    const formattedData = filteredData.map(item=>{
       return {
-        id: String(Math.random() * data.length),
+        id: String(new Date().getTime()) + item.amount,
         description: item.description,
-        amountFormatted: item.amount,
+        amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
-        dateFormated: item.date,
+        dateFormated: formatDate(item.date),
         tagColor: item.frequency === 'recorrente'? '#4E41F0':'#E44C4E',
       }
     })
-    setData(response)
-  },[]);
+
+    setData(formattedData);
+  },[listaData, monthSelected, yearSelected, data.length]);
   return (
     <Container>
       <ContentHeader title={title} lineColor={lineColor} >
-        <SelectInput options={months}/>
-        <SelectInput options={years}/>
+        <SelectInput options={months} onChange={(e)=> setMonthSelected(e.target.value)} defaultValue={monthSelected}/>
+        <SelectInput options={years} onChange={(e)=> setYearSelected(e.target.value)} defaultValue={yearSelected}/>
       </ContentHeader>
 
       <Filters>
@@ -84,7 +108,6 @@ const List: React.FC<IRouteParams> = ({match}) => {
       <Content>
         {
           data.map(item=>(
-            
             <HistoryFinanceCard
               key={item.id}
               tagColor={item.tagColor}
